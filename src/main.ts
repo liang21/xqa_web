7b421cf9100f428860c4b3661344a61a6416a389
liguangliang <13537223157@163.com>
2022-09-13 00:05:03 +0800

    feat: add mixin.scss

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
